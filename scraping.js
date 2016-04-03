e5f21a6d93e861e98bd5a81e148d3be171951cff
Ethan Weeks <ethanweeks@ymail.com>
2016-04-03 17:54:10 -0600

    basic scraping not saving return value

---
+++ scraping.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var request = require('request'),
+		cheerio = require('cheerio'),
+				 fs = require('fs');
+
+var events = [];
+
+request('http://www.colorado.com/events', function(err, res, body) {
+	if(!err && res.statusCode === 200) {
+
+		var $ = cheerio.load(body);
+		console.log($('span', 'div.item-list').text());
+	}
+});
